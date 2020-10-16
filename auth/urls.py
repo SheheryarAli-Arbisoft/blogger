3aced6ba3f221b8c70202e523fb2e6ebdd5a28b2
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-16 13:01:09 +0500

    Created the initial login route in auth app

--- auth/urls.py
+++ auth/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import login
 
 urlpatterns = [
-    path('', index)
+    path('login/', login)
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
-def index(request):
-    return HttpResponse('Auth App')
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        credentials = json.loads(request.body)
+        user = authenticate(
+            email=credentials['email'], password=credentials['password'])
+        if user is not None:
+            return HttpResponse('Login successful')
+        else:
+            return HttpResponse('Invalid credentials', status=401)
