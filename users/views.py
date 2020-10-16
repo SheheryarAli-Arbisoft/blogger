1d80d8b1eb5d8818e680241d08ec7b1a0ce086b0
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-16 17:55:37 +0500

    Added a route to login a user

--- users/views.py
+++ users/views.py
@@ -1,19 +1,39 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 import json
+
+User = get_user_model()
 
 
 @csrf_exempt
 def index(request):
-    data = json.loads(request.body)
+    if request.method == 'POST':
+        data = json.loads(request.body)
 
-    name, email, password = data['name'], data['email'], data['password']
+        name, email, password = data['name'], data['email'], data['password']
 
-    if not name or not email or not password:
-        return HttpResponse('All fields are required', 400)
+        if not name or not email or not password:
+            return HttpResponse('All fields are required', 400)
 
-    User = get_user_model()
-    user = User.objects.create_user(email=email, password=password, name=name)
+        user = User.objects.create_user(
+            email=email, password=password, name=name)
 
-    return HttpResponse(user)
+        return HttpResponse(user)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        email, password = data['email'], data['password']
+
+        if not email or not password:
+            return HttpResponse('All fields are required', 400)
+
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            return HttpResponse('Invalid credentials', status=401)
+        return HttpResponse('Login successful')
