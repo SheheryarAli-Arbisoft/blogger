6201f825f1bdd9c6b988bd0287aa82b4087a658a
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-17 21:34:19 +0500

    Added the blogs model and created relation in user serializer

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Blog(models.Model):
+    owner = models.ForeignKey(
+        get_user_model(), related_name='blogs', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from blogs.models import Blog
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(min_length=6)
+    blogs = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Blog.objects.all())
 
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password']
+        fields = ['id', 'name', 'email', 'password', 'blogs']
 
     def create(self, validated_data):
         name, email, password = validated_data['name'], validated_data['email'], validated_data['password']
