b3bbcdae2811e74a3caa4e2ccf9c74fb3648b922
SheheryarAli-Arbisoft <sheheryar.ali@arbisoft.com>
2020-10-19 15:41:29 +0500

    Created a private route component

---
+++ front-end/src/components/Routing/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { loadingSelector, isAuthenticatedSelector } from '../../selectors/auth';
+
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  const loading = useSelector(loadingSelector);
+  const isAuthenticated = useSelector(isAuthenticatedSelector);
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !loading && !isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to='/login' />
+        )
+      }
+    />
+  );
+};
---
+++ front-end/src/selectors/auth.js
@@ -0,0 +1,11 @@
+import { createSelector } from 'reselect';
+
+export const loadingSelector = createSelector(
+  state => state.auth.loading,
+  loading => loading
+);
+
+export const isAuthenticatedSelector = createSelector(
+  state => state.auth.isAuthenticated,
+  isAuthenticated => isAuthenticated
+);
